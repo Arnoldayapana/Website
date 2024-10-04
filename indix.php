60fdfa3ead96e8e7014d8b8078c47ec7aa6f0b48
Your Name <your_email@example.com>
2024-10-04 12:59:33 +0800

    comitt

---
+++ indix.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample</title>
+</head>
+
+<body>
+    <h1 class="text-center">Good Day!</h1>
+    <?php
+    include 'Form/form.php';
+    ?>
+</body>
+
+</html>
