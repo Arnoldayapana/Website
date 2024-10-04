1b040dfbba51aa9ce97b7ffa0316cea271737d90
Your Name <your_email@example.com>
2024-10-04 13:03:44 +0800

    dadawd

--- indix.php
+++ indix.php
@@ -4,14 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sample</title>
+    <title>Document</title>
 </head>
 
 <body>
-    <h1 class="text-center">Good Day!</h1>
-    <?php
-    include 'Form/form.php';
-    ?>
+    <div class="container">
+        <div class="row">
+            <h1>Hello</h1>
+            <p>Sample Code</p>
+        </div>
+    </div>
+
 </body>
 
 </html>
